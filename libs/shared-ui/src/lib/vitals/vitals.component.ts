01c9e3b7ba1dd8fdbfcf96cdd9555c0b5d390d19
Steph Symosko <steph.symosko@gmail.com>
2022-07-24 01:43:42 -0400

    redo dashboard home

--- libs/shared-ui/src/lib/vitals/vitals.component.ts
+++ libs/shared-ui/src/lib/vitals/vitals.component.ts
@@ -4,7 +4,7 @@ import { Vital } from '@stephsymosko-nx/data-access';
 @Component({
   selector: 'stephsymosko-nx-vitals',
   templateUrl: './vitals.component.html',
-  styleUrls: ['./vitals.component.css'],
+  styleUrls: ['./vitals.component.scss'],
 })
 export class VitalsComponent implements OnInit {
   @Input() vitals: Vital[] = [];
