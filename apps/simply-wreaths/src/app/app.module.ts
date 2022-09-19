084091e99969a07cbbf14dc0faf243156d696829
Steph Symosko <steph.symosko@gmail.com>
2022-09-19 18:17:59 -0400

    create top-bar and product-list components

--- apps/simply-wreaths/src/app/app.module.ts
+++ apps/simply-wreaths/src/app/app.module.ts
@@ -4,9 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [
+    AppComponent,
+    NxWelcomeComponent,
+    TopBarComponent,
+    ProductListComponent,
+  ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
